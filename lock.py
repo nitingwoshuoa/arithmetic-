7c0f9015e5112bcd88ca35f1a0ceb2b05d4251c3
nitingwoshuoa <41627998+nitingwoshuoa@users.noreply.github.com>
2018-09-12 13:58:08 +0800

    Create lock.py
    锁机制

---
+++ lock.py
@@ -0,0 +1,28 @@
+import threading
+import time
+
+class Num:
+    def __init__(self):
+        self.num = 0
+        self.lock = threading.Lock()
+    def add(self):
+        self.lock.acquire()#加锁，锁住相应的资源
+        self.num += 1
+        num = self.num
+        self.lock.release()#解锁，离开该资源
+        return num
+
+n = Num()
+class jdThread(threading.Thread):
+    def __init__(self,item):
+        threading.Thread.__init__(self)
+        self.item = item
+    def run(self):
+        time.sleep(2)
+        value = n.add()#将num加1，并输出原来的数据和+1之后的数据
+        print(self.item,value)
+
+for item in range(5):
+    t = jdThread(item)
+    t.start()
+    t.join()#使线程一个一个执行
